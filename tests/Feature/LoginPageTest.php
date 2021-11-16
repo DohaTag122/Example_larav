a921d6bd5fa29eb6894f04b3969d670ddc2a3cc4
DohaTag122 <dohatag53@gmail.com>
2021-11-16 03:42:16 +0200

    add unit test Gate

--- tests/Feature/LoginPageTest.php
+++ tests/Feature/LoginPageTest.php
@@ -16,9 +16,34 @@ class LoginPageTest extends TestCase
      */
     public function test_user_can_login_using_the_login_form()
     {
-        $user=User::factory()->create();
-        $response=$this->post('/login',['email'=>$user->email,'password'=>'password']);
-        $this->assertAuthenticated();
-        $response->assertRedirect('/');
+        $user=User::factory()->create();// create user
+
+        // like i write email and password in form and click submit
+        $response=$this->post('/login',['email'=>$user->email,'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+        $this->assertAuthenticated(); //check if you login or not
+        $response->assertRedirect('index'); // check if will redirect to index or not
+    }
+
+
+    public function test_user_can_not_access_admin_page()
+    {
+        $user=User::factory()->create();// create user
+
+        // like i write email and password in form and click submit
+        $response=$this->post('/login',['email'=>$user->email,'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+        $this->get('/admin/users');
+        $response->assertRedirect('index');
+    }
+
+    public function test_user_can_access_admin_page()
+    {
+        $user=User::factory()->create();// create user
+        $user->roles()->attach(1);
+
+        // like i write email and password in form and click submit
+        $this->post('/login',['email'=>$user->email,'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+        $response=  $this->get('/admin/users');
+        $response->assertSeeText('Users');
+        
     }
 }
